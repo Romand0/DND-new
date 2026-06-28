4efe22e6fc44132a071b7d47827f31932fd2855b
Romand0 <romand098@outlook.com>
2026-06-28 09:19:48 +0800

    Add Layout component for page structure

---
+++ src/components/Layout.tsx
@@ -0,0 +1,16 @@
+// DM Toolkit - Page Layout Component
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+
+export default function Layout() {
+  return (
+    <div className="min-h-screen dark:bg-bg-dark light:bg-bg-light">
+      <Navbar />
+      <main className="pt-16">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+          <Outlet />
+        </div>
+      </main>
+    </div>
+  );
+}
